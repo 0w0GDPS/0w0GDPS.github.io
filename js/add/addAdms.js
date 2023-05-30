d0bd457baa5d5ec1b2a68355d728950093d9ec6a
0w0GDPS <130572766+0w0GDPS@users.noreply.github.com>
2023-05-30 20:17:59 +0300

    Update addAdms.js

--- js/add/addAdms.js
+++ js/add/addAdms.js
@@ -1,4 +1,4 @@
 const addAdms = new AddItems();
 
-addAdms.addAdmin('私はあなたのお母さんを愛しています', 'GDPS Owner, lists Owner', './img/devs/sasha.png', '私はあなたのお母さんを愛しています#6603');
+addAdms.addAdmin('ангелина няшкр#4262', 'GDPS Owner', './img/devs/sasha.png', 'ангелина няшкр#4262');
 addAdms.addAdmin('Once', 'Site Developer', './img/devs/main.png', 'Once#7474');
