a349ca836ce35213692fea3b9adaaa82d4e2fa88
0w0GDPS <130572766+0w0GDPS@users.noreply.github.com>
2023-05-30 22:26:25 +0300

    Update addAdms.js

--- js/add/addAdms.js
+++ js/add/addAdms.js
@@ -1,4 +1,5 @@
 const addAdms = new AddItems();
 
 addAdms.addAdmin('ангелина няшкр#4262', 'GDPS Owner', 'https://cdn.discordapp.com/avatars/1028943869095710732/6f7733f14eebbf2f519ee88dfbdd4790.png?size=512', 'ангелина няшкр#4262');
-addAdms.addAdmin('Once', 'Site Developer', './img/devs/main.png', 'Once#7474');
+addAdms.addAdmin('Sitcunuri', 'Demonlist moderator', './img/devs/sitc.png', 'Sitcunuri#5311');
+addAdms.addAdmin('Main', 'Site Developer, lists owner', './img/devs/main.png', 'Once#7474');
