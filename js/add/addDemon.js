0641bc3cf80cd574ae99f9e2591a9c1c11f22ca2
0w0GDPS <130572766+0w0GDPS@users.noreply.github.com>
2023-05-30 20:14:24 +0300

    Update addDemon.js

--- js/add/addDemon.js
+++ js/add/addDemon.js
@@ -1,5 +1,6 @@
 const addDemon = new AddItems();
 
+addDemon.addLevel('sakupen hell v2', 'r1val', 99);
 addDemon.addLevel('Lout', 'vitek8', 69);
 addDemon.addLevel('Six Cubes', 'viteck8', 61);
 addDemon.addLevel('Slow Motion', 'Elytra Clutch', 88);
