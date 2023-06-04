cc5f5df250516b4e1f7a47e979a224bab6867027
Once <98696858+EvAyako@users.noreply.github.com>
2023-06-04 12:55:47 +0300

    Update addImposible.js

--- js/add/addImposible.js
+++ js/add/addImposible.js
@@ -1,3 +1,5 @@
 const addImposible = new AddItems();
 
 addImposible.addLevel('config', 'negativ', 119);
+addImposible.add('iron heart', 'negativ', 49);
+addImposible.addLevel('blackout', 'negativ', 37);
