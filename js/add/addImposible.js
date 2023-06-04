a0f8db24c7ab7948eec2e3031503ae55ce972ae6
0w0GDPS <130572766+0w0GDPS@users.noreply.github.com>
2023-06-04 20:46:46 +0300

    Update addImposible.js

--- js/add/addImposible.js
+++ js/add/addImposible.js
@@ -1,5 +1,5 @@
 const addImposible = new AddItems();
 
 addImposible.addLevel('config', 'negativ', 119);
-addImposible.add('iron heart', 'negativ', 49);
+addImposible.addLevel('iron heart', 'negativ', 49);
 addImposible.addLevel('blackout', 'negativ', 37);
