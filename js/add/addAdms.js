ad0e1dc0945bd7b2980ab8813b31843900394100
Once <98696858+EvAyako@users.noreply.github.com>
2023-06-05 23:27:42 +0300

    Update addAdms.js

--- js/add/addAdms.js
+++ js/add/addAdms.js
@@ -1,5 +1,4 @@
 const addAdms = new AddItems();
 
-addAdms.addAdmin('ангелина няшкр#4262', 'GDPS Owner', 'https://cdn.discordapp.com/avatars/1028943869095710732/6f7733f14eebbf2f519ee88dfbdd4790.png?size=512', 'ангелина няшкр#4262');
-addAdms.addAdmin('Sitcunuri', 'Demonlist moderator', './img/devs/sitc.png', 'Sitcunuri#5311');
+addAdms.addAdmin('私はあなたのお母さんを愛しています', 'GDPS Owner', './img/devs/sasha.png', '私はあなたのお母さんを愛しています#6603');
 addAdms.addAdmin('Main', 'Site Developer, lists owner', './img/devs/main.png', 'Once#7474');
