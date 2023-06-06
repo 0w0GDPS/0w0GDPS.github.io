4e261029d73883d33838bd8c90d1f3d0f8080b3a
Sitcunuri <109351496+sitcunuri@users.noreply.github.com>
2023-06-06 07:07:36 +0500

    Yeah

--- js/add/addDemon.js
+++ js/add/addDemon.js
@@ -1,4 +1,5 @@
 const addDemon = new AddItems();
 
+addDemon.addLevel('ChessIsHard', 'MagnusCarlsen', 1020312)
+addDemon.addLevel('Slow Motion', 'ElytraClutch', 88);
 addDemon.addLevel('Lout', 'vitek8', 69);
-addDemon.addLevel('Slow Motion', 'Elytra Clutch', 88);
