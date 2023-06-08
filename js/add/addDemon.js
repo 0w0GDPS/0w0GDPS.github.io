89f3ee31e9e5f4dd021c29267077b022718ca919
0w0GDPS <130572766+0w0GDPS@users.noreply.github.com>
2023-06-08 22:11:48 +0300

    Update addDemon.js

--- js/add/addDemon.js
+++ js/add/addDemon.js
@@ -1,5 +1,4 @@
 const addDemon = new AddItems();
 
-addDemon.addLevel('ChessIsHard', 'MagnusCarlsen', 1020312)
-addDemon.addLevel('Slow Motion', 'ElytraClutch', 88);
 addDemon.addLevel('Lout', 'vitek8', 69);
+addDemon.addLevel('Slow Motion', 'ElytraClutch', 88);
