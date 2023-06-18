5dfd555194ea5d300932aeef76b1bde518813a37
SNERONinactive <134524053+SNERONinactive@users.noreply.github.com>
2023-06-19 02:56:59 +0300

    Update addImposible.js

--- js/add/addImposible.js
+++ js/add/addImposible.js
@@ -1,4 +1,5 @@
 const addImposible = new AddItems();
 
+addImposible.addLevel('descended circles', 'negativ', 314);
 addImposible.addLevel('iron heart', 'negativ', 49);
 addImposible.addLevel('blackout', 'negativ', 37);
