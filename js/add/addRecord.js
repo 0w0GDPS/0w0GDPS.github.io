4ad6c0421da1d11790ecb435424820bcfb19737f
Once <98696858+EvAyako@users.noreply.github.com>
2023-06-23 21:58:37 +0300

    Update addRecord.js

--- js/add/addRecord.js
+++ js/add/addRecord.js
@@ -1,3 +1,13 @@
 const addRecord = new AddItems();
 
-addRecord.addRecord(37, 'negativ', '100%', 'https://youtu.be/iP_raS9u9a0', 'YouTube');
+addRecord.addRecord(69, 'Vitek08', '100%', '#', 'None');
+
+addRecord.addRecord(88, 'Elytra Clutch', '100%', '#', 'None');
+
+addRecord.addRecord(98, 'Den4ik', '100%', 'https://youtu.be/wPg1BVjWNts', 'YouTube');
+addRecord.addRecord(98, 'Aya', '100%', 'https://youtu.be/waPJK7rvGZU', 'YouTube');
+addRecord.addRecord(98, 'kripan', '100%', 'https://youtube.com/shorts/Q4g15z3lD6w?feature=share3', 'YouTube');
+
+addRecord.addRecord(106, 'Aya', '100%', 'https://www.youtube.com/embed/BqK4WWrEDvA', 'YouTube');
+
+addRecord.addRecord(314, 'Negativ', '100%', 'https://youtu.be/DBXHwM4C93A', 'YouTube');
