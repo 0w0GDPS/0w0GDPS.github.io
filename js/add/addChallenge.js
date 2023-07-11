f4daf6020888fc922625b949faf3211ab5aaf7ca
0w0GDPS <130572766+0w0GDPS@users.noreply.github.com>
2023-07-11 18:32:38 +0300

    Update addChallenge.js

--- js/add/addChallenge.js
+++ js/add/addChallenge.js
@@ -1,4 +1,5 @@
 const addChallenge = new AddItems();
 
 addChallenge.addLevel('ExoShere', 'Aya', 106);
+addChallenge.addLevel('Hella challo', 'BeeDeeS12', 207);
 addChallenge.addLevel('The silver', 'Den4ik', 98);
